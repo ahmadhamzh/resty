feabdacfcac5db924e9275e059cf5340468d316c
ahmadhamzh <ahmadhamzh@ymail.com>
2021-12-07 12:34:51 +0200

    add axios json pretty

--- src/app.js
+++ src/app.js
@@ -4,16 +4,14 @@ import "./app.scss";
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
-import Form from "./components/form/form";
-import Results from "./components/results/results";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import Form from "./components/form/index";
+import Results from "./components/results/index";
 
 const axios = require("axios");
 function App() {
   const [state, setState] = useState({ data: null, requestParams: {} });
-  // const [requestParams, setRequestParams] = useState({});
-
   const callApi = async (requestParams) => {
     let resApi;
     if (requestParams.method === "get") {
@@ -22,12 +20,8 @@ function App() {
     if (requestParams.method === "delete") {
       resApi = await axios.delete(requestParams.url);
     }
-    if (resApi.data.results) {
-      const data = {
-        count: resApi.data.results.length,
-        results: resApi.data.results
-      };
-      setState({ data, requestParams });
+    if (resApi) {      
+      setState({ data : resApi, requestParams });
     } else {
       setState({ data: "no data found", requestParams });
     }
@@ -40,7 +34,6 @@ function App() {
       <div>URL: {state.requestParams.url}</div>
       <Form handleApiCall={callApi} />
       <Results data={state.data} />
-
       <Footer />
     </React.Fragment>
   );
